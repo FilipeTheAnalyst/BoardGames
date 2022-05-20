edf693262ae4d28a859d203f1aca5cc2ba5c0ba0
FilipeTheAnalyst <fbalseiro@gmail.com>
2022-05-20 15:51:33 +0100

    Created pipeline to store data in SQLite DB

--- boardgames/boardgames/pipelines.py
+++ boardgames/boardgames/pipelines.py
@@ -6,8 +6,25 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlite3
 
 
 class BoardgamesPipeline:
+    def __init__(self):
+        self.con = sqlite3.connect('boardgames.db')
+        self.cur = self.con.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS games(
+        rank REAL PRIMARY KEY,
+        name VARCHAR(100),
+        url VARCHAR(255),
+        rating FLOAT
+        )""")
+
     def process_item(self, item, spider):
+        self.cur.execute("""INSERT OR IGNORE INTO games VALUES (?,?,?,?)""",
+                         (item['rank'], item['name'], item['url'], item['rating']))
+        self.con.commit()
         return item
