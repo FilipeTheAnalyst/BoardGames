f68a978d925b4f8dfe897053f236bac1216d7d28
FilipeTheAnalyst <fbalseiro@gmail.com>
2022-05-22 00:23:48 +0100

    Price tracker that sends notifications by email!

--- boardgames/boardgames/spiders/games.py
+++ boardgames/boardgames/spiders/games.py
@@ -1,5 +1,7 @@
 import scrapy
 from ..items import BoardgamesItem
+import pandas as pd
+import sqlite3
 
 
 class GamesSpider(scrapy.Spider):
@@ -9,29 +11,35 @@ class GamesSpider(scrapy.Spider):
     def parse(self, response):
         items = BoardgamesItem()
         base_url = 'https://boardgamegeek.com'
+        con = sqlite3.connect(
+            'C:/Users/u10054206/OneDrive - NOS SGPS, S.A/Carreira/Portfolio/Python Projects/Scrapy/boardgames/boardgames.db')
+        df = pd.read_sql_query('SELECT * FROM games;', con)
+        print(df['id'].values)
         for game in response.css('#row_'):
-            items['id'] = int(game.css("#row_ a::attr(href)").re("\d+")[0])
-            items['rank'] = game.css(".collection_rank a::attr(name)").get()
-            items['name'] = game.css(".primary ::text").get()
-            items['url'] = base_url + game.css("#row_ a::attr(href)").get()
-            items['rating'] = game.css(
-                "#row_ .collection_bggrating:nth-child(5)::text").get().split()[0]
-            try:
-                items['num_voters'] = int(game.css(
-                    "td.collection_bggrating ::text")[2].get().replace("\n", "").replace("\t", ""))
-            except:
-                print('N/A')
-            try:
-                items['year'] = int(game.css(
-                    "span.smallerfont.dull ::text").get()[1:-1])
-            except:
-                print('N/A')
-            try:
-                items['details'] = game.css(
-                    "p.smallefont.dull ::text").get().replace("\n", "").replace("\t", "")
-            except:
-                print('N/A')
-            yield items
+            if int(game.css("#row_ a::attr(href)").re("\d+")[0]) not in df['id'].values:
+                items['id'] = int(game.css("#row_ a::attr(href)").re("\d+")[0])
+                items['rank'] = game.css(
+                    ".collection_rank a::attr(name)").get()
+                items['name'] = game.css(".primary ::text").get()
+                items['url'] = base_url + game.css("#row_ a::attr(href)").get()
+                items['rating'] = game.css(
+                    "#row_ .collection_bggrating:nth-child(5)::text").get().split()[0]
+                try:
+                    items['num_voters'] = int(game.css(
+                        "td.collection_bggrating ::text")[2].get().replace("\n", "").replace("\t", ""))
+                except:
+                    items['num_voters'] = 'N/A'
+                try:
+                    items['year'] = int(game.css(
+                        "span.smallerfont.dull ::text").get()[1:-1])
+                except:
+                    items['year'] = 'N/A'
+                try:
+                    items['details'] = game.css(
+                        "p.smallefont.dull ::text").get().replace("\n", "").replace("\t", "")
+                except:
+                    items['details'] = 'N/A'
+                yield items
 
         next_page = response.css('a[title="next page"] ::attr(href)').get()
         if next_page is not None:
